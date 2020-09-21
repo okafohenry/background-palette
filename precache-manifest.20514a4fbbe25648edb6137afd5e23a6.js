67fb65fb833822035e07a631ace67845b6b48c5e
okafohenrie <okafohenrie@gmail.com>
2020-09-21 16:49:15 +0100

    Updates

---
+++ precache-manifest.20514a4fbbe25648edb6137afd5e23a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0fea8cd824febbde36c82610866ec3",
+    "url": "/background-palette/index.html"
+  },
+  {
+    "revision": "b648914c8bfc288b7dcf",
+    "url": "/background-palette/static/css/main.769509e1.chunk.css"
+  },
+  {
+    "revision": "a676d8bb30a3db793266",
+    "url": "/background-palette/static/js/2.8f8ad300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/background-palette/static/js/2.8f8ad300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b648914c8bfc288b7dcf",
+    "url": "/background-palette/static/js/main.b65e5d67.chunk.js"
+  },
+  {
+    "revision": "e5096b48f4654ff5dc82",
+    "url": "/background-palette/static/js/runtime-main.de4ccff1.js"
+  }
+]);
